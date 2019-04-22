8622f75a23289bfc9107c1f9827dcb319193106e
Shawn Ren <shawnren527@gmail.com>
2019-04-22 21:41:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
+# require_relative './views'
+
 class Application < Sinatra::Base
   # Write your code here!
+  set :views, './views'
+
+  get '/' do
+    erb :index
+  end
+
+  get '/greet' do
+    erb :greet
+  end
 
 end
